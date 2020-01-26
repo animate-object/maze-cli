d3621cf33f867895ec8e80231a2133f35b6be6ba
animate-object <alexander.f.prinsen@gmail.com>
2020-01-26 00:22:44 -0600

    struct opt based cli for maze generation

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+use exitfailure::ExitFailure;
+use maze;
+use structopt::clap::arg_enum;
+use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum AlgorithmArg {
+        AB, SW, BT
+    }
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum OutputTypeArg {
+        BIN, ASCII
+    }
+
+}
+
+arg_enum! {
+    #[derive(Debug, Clone)]
+    pub enum Corner {
+        NW, NE, SW, SE
+    }
+}
+
+impl Corner {
+    fn convert(&self) -> maze::Corner {
+        match &self {
+            Corner::NW => maze::Corner::NorthWest,
+            Corner::NE => maze::Corner::NorthEast,
+            Corner::SW => maze::Corner::SouthWest,
+            Corner::SE => maze::Corner::SouthEast,
+        }
+    }
+}
+
+arg_enum! {
+    #[derive(Debug, Clone)]
+    pub enum Dir {
+        N, E, S, W
+    }
+}
+
+impl Dir {
+    fn convert(&self) -> maze::Direction {
+        match &self {
+            Dir::N => maze::Direction::North,
+            Dir::E => maze::Direction::East,
+            Dir::S => maze::Direction::South,
+            Dir::W => maze::Direction::West,
+        }
+    }
+}
+
+#[derive(StructOpt, Debug)]
+struct MazeParams {
+    #[structopt(possible_values = &AlgorithmArg::variants(), case_insensitive = true)]
+    alg: AlgorithmArg,
+    #[structopt(short, long, default_value = "10")]
+    height: usize,
+    #[structopt(short, long, default_value = "10")]
+    width: usize,
+    #[structopt(short,long,  possible_values = &OutputTypeArg::variants(), case_insensitive = true , default_value = "ascii" )]
+    output_type: OutputTypeArg,
+
+    #[structopt(long, possible_values = &Corner::variants(), case_insensitive = true)]
+    corner: Option<Corner>,
+
+    #[structopt(long, possible_values = &Dir::variants(), case_insensitive = true)]
+    direction: Option<Dir>,
+}
+
+impl MazeParams {
+    fn derive_output_type(&self) -> maze::OutputType {
+        match &self.output_type {
+            OutputTypeArg::BIN => maze::OutputType::BIN,
+            OutputTypeArg::ASCII => maze::OutputType::ASCII,
+        }
+    }
+
+    fn derive_algorithm(&self) -> maze::Algorithm {
+        match &self.alg {
+            AlgorithmArg::AB => maze::Algorithm::AlduousBroder,
+            AlgorithmArg::SW => maze::Algorithm::SideWinder {
+                traversal_direction: self.derive_traversal_direction(),
+            },
+            AlgorithmArg::BT => maze::Algorithm::BinTree {
+                start_corner: self.derive_start_corner(),
+            },
+        }
+    }
+
+    fn derive_dimensions(&self) -> maze::Dimensions {
+        return maze::Dimensions {
+            height: self.height,
+            width: self.width,
+        };
+    }
+
+    fn derive_start_corner(&self) -> maze::Corner {
+        self.corner.as_ref().unwrap_or(&Corner::NW).convert()
+    }
+
+    fn derive_traversal_direction(&self) -> maze::Direction {
+        self.direction.as_ref().unwrap_or(&Dir::N).convert()
+    }
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let params: MazeParams = MazeParams::from_args();
+
+    let output: maze::Output = maze::generate(maze::Args {
+        dimensions: params.derive_dimensions(),
+        output_type: params.derive_output_type(),
+        algorigthm: params.derive_algorithm(),
+    })
+    .expect("Maze generation failed");
+    match output {
+        maze::Output::ASCII(text) => println!("{}", text),
+        maze::Output::BIN(bytes) => {
+            for byte in bytes {
+                print!("{:#08b} ", byte)
+            }
+        }
+    }
+    Ok(())
+}
